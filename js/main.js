89e196805e1bbd267890d175e7146263a9d0dec9
olutobi1996 <olutobi1996@icloud.com>
2026-03-17 21:41:32 +0000

    updates to website

--- js/main.js
+++ js/main.js
@@ -322,3 +322,23 @@ jQuery(document).ready(function($) {
 	});
 
 });
+
+document.querySelectorAll(".pro-card").forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (y - centerY) / 25;
+    const rotateY = -(x - centerX) / 25;
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "none";
+  });
+});
